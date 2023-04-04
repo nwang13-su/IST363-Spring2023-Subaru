dcb91bbd4000cef03d7b4d74a4fb9a8e477eea00
nwang13-su <54637573+nwang13-su@users.noreply.github.com>
2023-04-04 18:21:57 -0400

    sass foundation

---
+++ components/SectionCTA.js
@@ -0,0 +1,8 @@
+import styles from './sectioncta.module.scss';
+
+const SectionCTA = () => {
+    return <section className={styles.sectioncta}>
+        Call to action goes here
+    </section>
+}
+export default SectionCTA;

e226dc8cdaf8000052f331287f6a76e855b31f3e
nwang13-su <54637573+nwang13-su@users.noreply.github.com>
2023-04-18 17:55:33 -0400

    update file

--- pages/vehicles/[id].js
+++ pages/vehicles/[id].js
@@ -1,3 +1,55 @@
+import Image from 'next/image'
+
+import { getAllVehicleSlugs, getVehicleDataBySlug } from '../../lib/api'
+
+export async function getStaticPaths() {
+    const vehicles = await getAllVehicleSlugs();
+    console.log({vehicles});
+    const paths = vehicles.map((vehicle) => {
+        return {
+            params: {
+                id: vehicle.node.slug
+            }
+        }
+    })
+    
+    return {
+      paths: paths,
+      fallback: false, // can also be true or 'blocking'
+    }
+  }
+  
+  // `getStaticPaths` requires using `getStaticProps`
+  export async function getStaticProps({params}) {
+    const { id } = params;
+    console.log({id});
+    const vehicleData = await getVehicleDataBySlug(id);
+    return {
+      // Passed to the page component as props
+      props: { 
+        vehicleData
+      },
+    }
+  }
+  
+  export default function SingleVehiclePage({ vehicleData }) {
+    const {title, featuredImage} = vehicleData;
+    return <div>
+        <h1>{title}</h1>
+        {featuredImage && 
+          <Image 
+          src={featuredImage.node.sourceUrl}
+          alt={featuredImage.node.altText}
+          width={featuredImage.node.mediaDetails.width}
+          height={featuredImage.node.mediaDetails.height}
+          />
+        }
+    </div>
+  }
+
+
+
+/*
 import Image from "next/image";
 import Link from "next/link";
 import Layout from "../../components/Layout"
@@ -51,3 +103,5 @@ const SingleCarTemplate = ({data}) => {
     </Layout>
 }
 export default SingleCarTemplate;
+
+*/
--- pages/vehicles/index.js
+++ pages/vehicles/index.js
@@ -1,3 +1,32 @@
+import { getAllVehicles } from '../../lib/api';
+import Layout from '../../components/Layout';
+import Link from 'next/link'
+
+export async function getStaticProps() {
+    const vehiclesData = await getAllVehicles();
+    return {
+      props: {
+        vehiclesData
+      }, // will be passed to the page component as props
+    }
+  }
+
+const VehiclesPage = ({vehiclesData}) => {
+    return (
+        <Layout vehicles={vehiclesData}>
+        <h1>Vehicles</h1>
+        {vehiclesData.map((vehicle, index) => {
+          const {slug, title} = vehicle.node;
+            return <div key={index}>
+                <h2><Link href={`/vehicles/${slug}`}>{title}</Link></h2>
+            </div>
+        })}
+        </Layout>
+    )
+}
+export default VehiclesPage
+
+/*
 import Image from "next/image";
 import Layout from "../../components/Layout"
 import Link from 'next/link';
@@ -36,3 +65,4 @@ const VehiclesPage = ({ data }) => {
     </Layout>
 }
 export default VehiclesPage
+*/

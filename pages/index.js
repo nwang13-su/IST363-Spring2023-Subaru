6863da8ad5e13a3803fc6665008256bf05c1bd29
nwang13-su <54637573+nwang13-su@users.noreply.github.com>
2023-05-15 21:23:10 -0400

    useEffect added

--- pages/index.js
+++ pages/index.js
@@ -1,46 +1,20 @@
 import { useState } from 'react';
 
-import Button from '../components/Button';
-//import CarColorPicker from '../components/OLDCarColorPicker';
-import Header from '../components/Header';
-import Image from 'next/image';
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+
 import Layout from '../components/Layout';
-import LocationPicker from '../components/LocationPicker';
-import SectionCTA from '../components/SectionCTA';
-import Showcase from '../components/Showcase';
 
-import { 
-  //getAllVehicles, 
-  //getAllColors 
-} from '../lib/api';
 
-export async function getStaticProps() {
-    // Get external data from the file system, API, DB, etc.
-    //const data = getAllVehicles();
-    //const colors = getAllColors();
-    
-    // The value of the 'props' key will be
-    // passed to the 'Home' component
-  return {
-      props: {
-        //data,
-        //colors
-    }    
-  }
-}
-const Homepage = ({
-  //data, 
-  //colors
-}) => {
-  //console.log({data});
+const Homepage = ({}) => {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push(`/vehicles`);
+  }, [])
+
   return <Layout>
-  
-    {
-      //<Showcase />
-      //<LocationPicker />
-      //<CarColorPicker colors={colors}/>
-      //<SectionCTA />
-    }
+
     
     </Layout>
 }
